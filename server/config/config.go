1d9df5d32f09854c5b87d7b12364be7025980222
Yonatan Harel <yoharel.dev@gmail.com>
2025-12-06 09:58:10 +0200

    fixed errors in config.go

--- server/config/config.go
+++ server/config/config.go
@@ -7,30 +7,30 @@ import (
 )
 
 type Config struct {
-	Server sturct {
+	Server struct {
 		Port 			int `yaml:"port"`
 		ReadTimeoutMS 	int `yaml:"read_timeout_ms"`
 		WriteTimeoutMS 	int `yaml:"write_timeout_ms"`
 		MaxConnections 	int `yaml:"max_connections"`
-		Workerschan  	int `yaml:"workers"`
+		Workers  		int `yaml:"workers"`
 	} `yaml:"server"`
 
 	Logging struct {
-		Level string `yaml:"level"`
-		Format string `yaml:"format"`
-		DisableReqLogs bool `yaml:"disable_request_logs"`
+		Level 			string `yaml:"level"`
+		Format 			string `yaml:"format"`
+		DisableReqLogs 	bool `yaml:"disable_request_logs"`
 	} `yaml:"logging"`
 
 	Metrics struct {
-		Enabled bool `yaml:"enabled"`
-		Port    int  `yaml:"port"`
-		Path   string `yaml:"path"`
+		Enabled 		bool `yaml:"enabled"`
+		Port    		int  `yaml:"port"`
+		Path   			string `yaml:"path"`
 	} `yaml:"metrics"`
 
 	Performance struct {
-		ParseJSON bool `yaml:"parse_json"`
-		IncludeTimestamp bool `yaml:"include_timestamp"`
-		MinimizeResponse bool `yaml:"minimize_response"`
+		ParseJSON 			bool `yaml:"parse_json"`
+		IncludeTimestamp 	bool `yaml:"include_timestamp"`
+		MinimizeResponse 	bool `yaml:"minimize_response"`
 	} `yaml:"performance"`
 }
 
